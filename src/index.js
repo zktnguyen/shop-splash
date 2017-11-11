d83a0aac3dd553a1864d1b64b3169093da14d5d0
zktnguyen <zktnguyen@ucdavis.edu>
2017-11-11 04:04:40 -0800

    added favicon

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser';
 import Promise from 'bluebird';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import favicon from 'serve-favicon';
 
 import router from './routes';
 
@@ -20,6 +21,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 mongoose.Promise = Promise;
 mongoose.connect(
