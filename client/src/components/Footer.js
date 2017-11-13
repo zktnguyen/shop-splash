eecad3d14a32c7f5e1def68fca7115efb28ab6f5
zktnguyen <zktnguyen@ucdavis.edu>
2017-11-12 23:12:44 -0800

    font change and github link provided at footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,14 @@ import React from 'react';
 const Footer = () => (
   <footer className="text-center pullDown">
     <div className="container">
-      <p>By Kim</p>
+      <p>
+        By Kim.{' '}
+        <span>
+          <a href="https://github.com/zktnguyen/shop-splash">
+            GitHub Repository
+          </a>
+        </span>
+      </p>
     </div>
   </footer>
 );
