0db73fe460ea407788a63620b913855d0b14cfd5
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-11-13 19:19:28 -0800

    Remove redux-logger for production

--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,14 @@ import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import thunk from 'redux-thunk'; // eslint-disable-next-line
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 
 import './index.css';
 import App from './App';
 import Reducers from './reducers';
 import registerServiceWorker from './registerServiceWorker';
 
-const middleware = applyMiddleware(thunk, logger);
+const middleware = applyMiddleware(thunk); // add logger for development
 const store = createStore(Reducers, middleware);
 
 render(
