b78766b4703142d2cde51f70b36ab5ea6f0d01fe
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-11-14 21:40:12 -0800

    More methods added for user model, to create authentication route next

---
+++ src/models/user.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import uniqueValidator from 'mongoose-unique-validator';
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    index: true,
+    unique: true
+  },
+  passwordHash: { type: String, required: true }
+});
+
+export function cryptPassword(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+}
+
+export function comparePassword(password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+}
+
+userSchema.methods.isValidPassword = function isValidPassword(password) {
+  return comparePassword(password);
+};
+
+userSchema.methods.setPassword = function setPassword(password) {
+  this.passwordHash = cryptPassword(password);
+};
+
+// creates a json web token with email given
+userSchema.methods.generateJWT = function generateJWT() {
+  return jwt.sign(
+    {
+      email: this.email
+    },
+    'secretkey20010'
+  );
+};
+
+// This returns an object with the email of the user logging in / signing up
+// token helps identify if the user is authenticated.
+userSchema.methods.toAuthJSON = function toAuthJSON() {
+  return {
+    email: this.email,
+    token: this.generateJWT()
+  };
+};
+
+const User = mongoose.model('User', userSchema);
+export default User;
